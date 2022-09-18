3be13730d7d7e3ab877759be44f3340e98de36b4
Francesco Fucci <fr.fucci.univ@gmail.com>
2022-09-18 15:26:58 +0200

    add llvm-tools-preview.

--- .github/workflows/scripts/run_coverage.sh
+++ .github/workflows/scripts/run_coverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xeu
 
+rustup component add llvm-tools-preview
 RUSTFLAGS="-Cinstrument-coverage" cargo test --all-targets
 grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing -o ./coverage/ --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
 Footer
