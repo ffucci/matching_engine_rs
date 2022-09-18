357c57de3db02cf993d93901222519c1b3d3e15f
Francesco Fucci <fr.fucci.univ@gmail.com>
2022-09-18 13:35:25 +0200

    Cleanup main.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,4 @@ mod data_types;
 mod order_book;
 mod matching;
 
-fn main() {
-    let order = data_types::Order{id:1, side: data_types::Side::Buy, price:12.2f32, qty:100};
-    println!("{:?}", order);
-}
+fn main() {}
